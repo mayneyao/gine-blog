1c51f726e7d93276284c1bbc84826d39c4cc8492
mayne <dreama9ain@gmail.com>
2019-01-10 16:23:15 +0800

    share link open in blank tab & update readme

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 又一个blog轮子，基于gatsby,react,material-ui
 
+
+## 项目进展
+
+[https://www.notion.so/gine/gatsby-blog-ae7fc5a6164f478982f8f9e38b2c5e23](https://www.notion.so/gine/gatsby-blog-ae7fc5a6164f478982f8f9e38b2c5e23)
+
 ## TODO
 
 + 列表页
--- src/components/link-without-underline.js
+++ src/components/link-without-underline.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default (props) => (
-    <a href={props.href} style={{textDecoration: 'none'}}>
+    <a href={props.href} style={{textDecoration: 'none'}} target='__blank'>
         {props.text}
     </a>
 )
