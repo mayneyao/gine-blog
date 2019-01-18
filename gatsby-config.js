1424f835c346cc540b19d369720afea5ebaac969
mayne <dreama9ain@gmail.com>
2019-01-18 10:40:49 +0800

    del conf comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,16 @@ module.exports = {
         title: `Mayne's Blog`,
         pageSize: 3,
         description: `All things about Mayne`,
-        siteUrl: `https://gine.me`
+        siteUrl: `https://gine.me`,
     },
     plugins: [
         {
-            resolve: `gatsby-plugin-feed`
+            resolve: `gatsby-plugin-feed`,
         },
         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
-                trackingId: "UA-89592481-3",
+                trackingId: 'UA-89592481-3',
                 head: true,
             },
         },
@@ -36,8 +36,8 @@ module.exports = {
                       }
                     }
                   }
-              }`
-            }
+              }`,
+            },
         },
         {
             resolve: `gatsby-plugin-manifest`,
@@ -47,8 +47,6 @@ module.exports = {
                 start_url: `/`,
                 background_color: `#ffffff`,
                 theme_color: `#ffffff`,
-                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
                 display: `standalone`,
                 icon: `src/static/41546411364_.pic.jpg`, // This path is relative to the root of the site.
             },
@@ -82,45 +80,16 @@ module.exports = {
                     {
                         resolve: `gatsby-remark-images`,
                         options: {
-                            // It's important to specify the maxWidth (in pixels) of
-                            // the content container as this plugin uses this as the
-                            // base for generating different widths of each image.
                             maxWidth: 590,
                         },
                     },
                     {
                         resolve: `gatsby-remark-prismjs`,
                         options: {
-                            // Class prefix for <pre> tags containing syntax highlighting;
-                            // defaults to 'language-' (eg <pre class="language-js">).
-                            // If your site loads Prism into the browser at runtime,
-                            // (eg for use with libraries like react-live),
-                            // you may use this to prevent Prism from re-processing syntax.
-                            // This is an uncommon use-case though;
-                            // If you're unsure, it's best to use the default value.
                             classPrefix: 'language-',
-                            // This is used to allow setting a language for inline code
-                            // (i.e. single backticks) by creating a separator.
-                            // This separator is a string and will do no white-space
-                            // stripping.
-                            // A suggested value for English speakers is the non-ascii
-                            // character '›'.
-                            inlineCodeMarker: { sh: 'bash' },
-                            // This lets you set up language aliases.  For example,
-                            // setting this to '{ sh: "bash" }' will let you use
-                            // the language "sh" which will highlight using the
-                            // bash highlighter.
+                            inlineCodeMarker: {sh: 'bash'},
                             aliases: {},
-                            // This toggles the display of line numbers globally alongside the code.
-                            // To use it, add the following line in src/layouts/index.js
-                            // right after importing the prism color scheme:
-                            //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
-                            // Defaults to false.
-                            // If you wish to only show line numbers on certain code blocks,
-                            // leave false and use the {numberLines: true} syntax below
                             showLineNumbers: false,
-                            // If setting this to true, the parser won't handle and highlight inline
-                            // code used in markdown i.e. single backtick code like `this`.
                             noInlineHighlight: false,
                         },
                     },
