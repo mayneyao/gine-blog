1a0884f9cd3ad6b834625857d114968754c83bab
mayne <dreama9ain@gmail.com>
2019-06-01 21:24:39 +0800

    add notion lambda function for notion api

---
+++ src/lambda/notion.js
@@ -0,0 +1,12 @@
+
+const notion = require('../notion/api')
+
+exports.handler = async (event, context) => {
+    let notionUrl = event.queryStringParameters.url
+    let response = await queryCollection(notionUrl)
+    return {
+        headers: { "Content-Type": "application/json;charset=UTF-8" },
+        statusCode: 200,
+        body: JSON.stringify(response)
+    }
+}
