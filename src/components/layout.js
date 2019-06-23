e9921337ea99a13e496442c885cc688cb849c930
mayne <dreama9ain@gmail.com>
2019-06-23 22:19:00 +0800

    fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -107,10 +107,12 @@ class Layout extends React.Component {
                         config.google_ad_client.open && <>
                             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                             <script>
-                                (adsbygoogle = window.adsbygoogle || []).push({
-                                    google_ad_client: `"${config.google_ad_client.clientId}"`,
-                            enable_page_level_ads: true
-                        });
+                                {
+                                    (adsbygoogle = window.adsbygoogle || []).push({
+                                        google_ad_client: `"${config.google_ad_client.clientId}"`,
+                                        enable_page_level_ads: true
+                                    })
+                                }
                             </script>
                         </>
                     }
