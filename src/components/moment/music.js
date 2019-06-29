64119ba95cdcc94b57d2a8b2b3e7d744cebcdd85
mayne <dreama9ain@gmail.com>
2019-06-29 20:40:15 +0800

    fix moments card

--- src/components/moment/music.js
+++ src/components/moment/music.js
@@ -1,27 +1,18 @@
 import React from 'react';
 
 
-export default function Music(props) {
-    const { link, type } = props
-    console.log(type)
-    if (type) {
-        switch (type) {
-            case 'music':
-                console.log(type)
-                let url = new URL(link)
-                if (url.hostname == "open.spotify.com") {
-                    let pathList = url.pathname.split('/')
-                    let trackID = pathList[pathList.length - 1]
-                    return (
-                        <iframe src={`https://open.spotify.com/embed/track/${trackID}`} width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                    )
-                } else {
-                    return (
-                        <div>{link}</div>
-                    )
-                }
-        }
+export default function MomentMusicCard(props) {
+    const { link } = props
+    let url = new URL(link)
+    if (url.hostname == "open.spotify.com") {
+        let pathList = url.pathname.split('/')
+        let trackID = pathList[pathList.length - 1]
+        return (
+            <iframe src={`https://open.spotify.com/embed/track/${trackID}`} width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        )
     } else {
-        return (<></>)
+        return (
+            <div>{link}</div>
+        )
     }
 }
