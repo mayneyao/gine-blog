c24d8ea336b839b61e3fdf6c38d08f9b8a245920
mayne <dreama9ain@gmail.com>
2019-09-10 19:01:10 +0800

    fix duplicate data fetch

--- gatsby-theme-gine-blog/gatsby-config.js
+++ gatsby-theme-gine-blog/gatsby-config.js
@@ -9,9 +9,6 @@ module.exports = {
         },
         {
             resolve: `gatsby-source-notion-database`,
-            options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
-            }
         }
     ]
 }
