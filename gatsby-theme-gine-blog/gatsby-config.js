546b06318ea7d08b1b51d32c7fb4129f0078b733
mayne <dreama9ain@gmail.com>
2019-09-12 04:58:19 +0800

    fix

--- gatsby-theme-gine-blog/gatsby-config.js
+++ gatsby-theme-gine-blog/gatsby-config.js
@@ -1,5 +1,11 @@
 module.exports = {
     plugins: [
         `gatsby-plugin-react-helmet`,
+        {
+            resolve: "gatsby-plugin-netlify-cache",
+            options: {
+                cachePublic: true
+            }
+        },
     ]
 }
